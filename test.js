99f32f0cdf2c09b5cd7e3ed5af3928a17af0cbe3
heluyao <heluyaosj13@gmail.com>
2013-06-27 12:36:17 +0800

    6.27

--- test.js
+++ test.js
@@ -1,12 +1,23 @@
 var css = require("stylus"), 
+    fs = require("fs"),
     str = require("fs").readFileSync(__dirname +"/test.styl", "utf8");
 	
 css.render(str, { filename: "test.styl" }, function(err, css) {
     if (err) throw err;
-    var http = require('http');
+    //写入css文件
+	fs.open("test.css","w",0644,function(e,fd){
+      if(e) throw e;
+      fs.write(fd,css,0,'utf8',function(e){
+        if(e) throw e;
+        fs.closeSync(fd);
+      });
+    });
+	
+	//创建http
+	var http = require('http');
     http.createServer(function (req, res) {
         res.writeHead(200, {'Content-Type': 'text/css'});
         res.end(css);
     }).listen(8888, '127.0.0.1');
-    console.log('已经启2动 http://127.0.0.1:8888/');
+    console.log('已经启动222 http://127.0.0.1:8888/');
 });
